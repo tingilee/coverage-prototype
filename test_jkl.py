c64a857737b7cd142d96606d52bc634f5472c07c
jacqueline.lee <jacqueline.lee@robinhood.com>
2023-09-25 09:55:58 -0700

    [coveragerc] not working

---
+++ test_jkl.py
@@ -0,0 +1,14 @@
+import sys
+import os  # pragma: no cover
+
+def test_arc_patch():
+    print("test_arc_patch")
+    x = 3 + 4
+    if x == 7:
+        a = "x"
+    if True:
+        print("hiihihi")
+    else:
+        print("ho")
+
+
